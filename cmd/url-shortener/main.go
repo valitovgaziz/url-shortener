8a9d6de981f149e6845aef9ed0b5ef5eba5b7807
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-30 12:25:31 +0500

    config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,9 +1,26 @@
-package mai
+package main
 
 import (
-	
+	"encoding/json"
+	"log/slog"
+	"os"
+
+	"github.com/valitovgaziz/url-shortener/internall/config"
 )
 
+func init() {
+	os.Setenv("CONFIG_PATH", "D:\\GoLang\\url-shortener\\config\\local.yaml")
+}
+
 func main() {
-	// 
+
+	// load config
+	cfg := config.MustLoad()
+	
+	strg, err := json.Marshal(cfg)
+	if err!= nil {
+		slog.Error("error", "error", err)
+		return
+	}
+	slog.Info("config", "config", string(strg))
 }
